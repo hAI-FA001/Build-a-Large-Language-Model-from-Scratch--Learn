dcf86e081fff17dd7df3f02084392cc7cbc072e8
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-08-25 14:58:09 +0500

    code from chapters

---
+++ Bonus/deps/gpt_model.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn as nn
+
+from .mha import MultiHeadAttention
+
+class GELU(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, x):
+        # approximation
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0/torch.pi)) *
+            (x + 0.044715*torch.pow(x,3))
+        ))
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        
+        self.layers = nn.Sequential(
+            nn.Linear(cfg['emb_dim'], 4*cfg['emb_dim']),
+            GELU(),
+            nn.Linear(4*cfg['emb_dim'], cfg['emb_dim']),
+        )
+    
+    def forward(self, x):
+        return self.layers(x)
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+
+        self.eps = 1e-5
+        # trainable params so the LLM can adjust
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+    
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        # 
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)  # eps to prevent division by 0
+
+        return self.scale*norm_x + self.shift
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+        self.attn = MultiHeadAttention(
+            d_in=cfg['emb_dim'],
+            d_out=cfg['emb_dim'],
+            context_len=cfg['context_len'],
+            num_heads=cfg['n_heads'],
+            dropout=cfg['drop_rate'],
+            qkv_bias=cfg['qkv_bias'],
+        )
+
+        self.ff = FeedForward(cfg)
+        
+        self.norm1 = LayerNorm(cfg['emb_dim'])
+        self.norm2 = LayerNorm(cfg['emb_dim'])
+
+        self.drop_shortcut = nn.Dropout(cfg['drop_rate'])
+    
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.attn(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        return x
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg) -> None:
+        super().__init__()
+
+        self.tok_emb = nn.Embedding(cfg['vocab_size'], cfg['emb_dim'])
+        self.pos_emb = nn.Embedding(cfg['context_len'], cfg['emb_dim'])
+        
+        self.drop_emb = nn.Dropout(cfg['drop_rate'])
+
+        self.trf_blocks = nn.Sequential(*[
+            TransformerBlock(cfg)
+            for _ in range(cfg['n_layers'])
+        ])
+
+        self.final_norm = LayerNorm(cfg['emb_dim'])
+        self.out_head = nn.Linear(cfg['emb_dim'], cfg['vocab_size'], bias=False)
+    
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+
+        tok_embs = self.tok_emb(in_idx)
+        pos_embs = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+
+        x = tok_embs + pos_embs
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        
+        logits = self.out_head(x)
+        return logits
