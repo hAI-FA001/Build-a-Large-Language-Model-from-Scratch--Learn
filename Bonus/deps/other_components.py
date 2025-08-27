4d27fb1b108cb172618991a9b3beab5bcbc6677c
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-08-27 13:31:40 +0500

    add Qwen RMSNorm and GQA

--- Bonus/deps/other_components.py
+++ Bonus/deps/other_components.py
@@ -18,6 +18,30 @@ class RMSNorm(nn.Module):
         # normalize input by this
         return (x_normed * self.weight).to(dtype=x.dtype)
 
+class RMSNorm_Qwen(nn.Module):
+    def __init__(self, emb_dim, eps=1e-6, bias=False):
+        super().__init__()
+
+        self.eps = eps
+        # Llama had only 1 parameter
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim)) if bias else None
+    
+    def forward(self, x):
+        input_dtype = x.dtype
+        x = x.to(torch.float32)  # for Qwen
+        
+        # RMSNorm
+        variance = x.pow(2).mean(dim=-1, keepdim=True)
+        norm_x = x * torch.rsqrt(variance + self.eps)
+
+        # new
+        norm_x = norm_x * self.scale
+        if self.shift is not None:
+            norm_x = norm_x + self.shift
+        
+        return norm_x.to(input_dtype)
+
 class SiLU(nn.Module):
     def __init__(self):
         super(SiLU, self).__init__()
@@ -99,3 +123,74 @@ def compute_rope(x, cos, sin):
 
     return x_rotated.to(dtype=x.dtype)
 
+class GroupedQueryAttention_Qwen(nn.Module):
+    def __init__(self, d_in, num_heads, num_kv_groups,
+                    head_dim=None, dtype=None):
+        super().__init__()
+        assert num_heads % num_kv_groups == 0, 'num_heads must be divisible by num_kv_groups'
+
+        self.num_heads = num_heads
+        self.num_kv_groups = num_kv_groups
+        self.group_size = num_heads // num_kv_groups
+
+        if head_dim is None:
+            assert d_in % num_heads == 0, 'd_in must be divisible by num_heads if head_dim is not provided'
+            head_dim = d_in // num_heads
+        self.head_dim = head_dim
+        # if head_dim not provided then:
+        #   d_out = num_heads * head_dim = num_heads * (d_in / num_heads) = d_in
+        #   so, d_out = d_in
+        self.d_out = num_heads*head_dim
+
+        self.W_q = nn.Linear(d_in, self.d_out, bias=False, dtype=dtype)
+        # if head_dim not provided, then:
+        #   num_kv_groups * head_dim = num_kv_groups * d_in / num_heads
+        #       = (num_kv_groups / num_heads) * d_in
+        #       = (1 / group_size) * d_in = d_in / group_size
+        # in GQA, K and V are repeated "group_size" times, so:
+        #       = group_size * ((1 / group_size) * d_in)
+        #       = d_in
+        self.W_k = nn.Linear(d_in, num_kv_groups*head_dim, bias=False, dtype=dtype)
+        self.W_v = nn.Linear(d_in, num_kv_groups*head_dim, bias=False, dtype=dtype)
+
+        self.out_proj = nn.Linear(self.d_out, d_in, bias=False, dtype=dtype)
+
+        # new, QK-Norm
+        self.q_norm = RMSNorm_Qwen(head_dim, eps=1e-6)
+        self.k_norm = RMSNorm_Qwen(head_dim, eps=1e-6)
+    
+    def forward(self, x, mask, cos, sin):
+        b, num_tokens, _ = x.shape
+
+        queries = self.W_q(x)  # (b, num_tokens, num_heads * head_dim)
+        keys = self.W_k(x)  # (b, num_tokens, num_kv_groups * head_dim)
+        values = self.W_v(x)  # (b, num_tokens, num_kv_groups * head_dim)
+
+        # "un-flatten" last dims
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+        keys = keys.view(b, num_tokens, self.num_kv_groups, self.head_dim)
+        values = values.view(b, num_tokens, self.num_kv_groups, self.head_dim)
+        
+        queries = queries.transpose(1,2)
+        keys = keys.transpose(1,2)
+        values = values.transpose(1,2)
+
+        # new
+        queries = self.q_norm(queries)
+        keys = self.k_norm(keys)
+
+        queries = compute_rope(queries, cos, sin)
+        keys = compute_rope(keys, cos, sin)
+
+        # (..., head_dim, num_kv_groups) -> (..., head_dim, num_heads)
+        #   num_heads / num_kv_groups = group_size
+        #   => num_heads = num_kv_groups * group_size
+        keys = keys.repeat_interleave(self.group_size, dim=1)
+        values = values.repeat_interleave(self.group_size, dim=1)
+
+        attn_scores = queries @ keys.transpose(2,3)
+        attn_scores = attn_scores.masked_fill(mask, -torch.inf)
+        attn_weights = torch.softmax(attn_scores / self.head_dim**0.5, dim=-1)
+
+        context = (attn_weights @ values).transpose(1,2).reshape(b, num_tokens, self.d_out)
+        return self.out_proj(context)
